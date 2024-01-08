ae18ab8767e7e44fb7282b47ea3e76f660f0a71d
Valow <80098893+valow-psd@users.noreply.github.com>
2024-01-08 10:09:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const multer  = require('multer');
-const upload = multer({ dest: './images/' });
+const multer = require('multer');
+const upload = multer({ dest: '/tmp/images/' });
 
 const app = express();
 const port = 3000;
