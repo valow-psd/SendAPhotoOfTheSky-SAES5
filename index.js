20b5a687b859f3bced86d926d22f1befe7f3a6a6
Valow <80098893+valow-psd@users.noreply.github.com>
2024-01-08 10:38:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer({ dest: '/tmp/images/' }); // Assurez-vous que ce dossier est accessible
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-// Middleware pour le parsing des données JSON. Vous devrez ajouter un parser pour les données multipart.
-app.use(bodyParser.json());
+// Route pour le téléchargement des images
+app.post('/upload', upload.single('photo'), (req, res) => {
+    console.log(req.file);
+    res.send(`Image téléchargée avec succès: ${req.file.filename}`);
+});
 
-app.post('/upload', (req, res) => {
-    // Ici, vous devrez gérer la réception du fichier manuellement.
-    // Cet exemple est très simplifié et suppose que vous recevez les données de l'image directement.
-    // Dans la pratique, il serait beaucoup plus complexe de gérer les données multipart/form-data.
+// Route pour lister toutes les images téléchargées
+app.get('/list-images', (req, res) => {
+    const directoryPath = path.join(__dirname, '/tmp/images/');
 
-    const imageData = req.body.image; // Supposons que l'image est envoyée en tant que chaîne base64
-    const buffer = Buffer.from(imageData, 'base64');
-    const filePath = path.join(__dirname, '/tmp/images/', 'uploaded_image.jpg');
-
-    fs.writeFile(filePath, buffer, (err) => {
+    fs.readdir(directoryPath, function (err, files) {
         if (err) {
-            return res.status(500).send('Error saving the image');
+            res.status(500).send('Unable to scan directory: ' + err);
+            return;
         }
-        res.send('Image téléchargée avec succès.');
+        let fileList = files.map(file => {
+            return { name: file };
+        });
+        res.send(fileList);
     });
 });
 
+// Route pour récupérer une image spécifique
 app.get('/image/:filename', (req, res) => {
     const filename = req.params.filename;
     const filePath = path.join(__dirname, '/tmp/images/', filename);
@@ -33,13 +37,13 @@ app.get('/image/:filename', (req, res) => {
     fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
             res.status(404).send('File not found');
-        } else {
-            res.sendFile(filePath);
+            return;
         }
+        res.sendFile(filePath);
     });
 });
 
-
+// Démarrage du serveur
 app.listen(port, () => {
     console.log(`Serveur en écoute sur le port ${port}`);
 });
