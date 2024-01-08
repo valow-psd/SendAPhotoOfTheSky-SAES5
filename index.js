47c21917c16733597e5170fbcdec7560e9a40249
Valow <80098893+valow-psd@users.noreply.github.com>
2024-01-08 10:17:14 +0100

    Add route to get the image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: '/tmp/images/' });
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,23 @@ app.post('/upload', upload.single('photo'), (req, res) => {
     console.log(req.file);
     res.send('Image téléchargée avec succès.');
 });
+
+app.get('/list-images', (req, res) => {
+    const directoryPath = path.join(__dirname, '/tmp/images/');
+
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            res.status(500).send('Unable to scan directory: ' + err);
+        } 
+        else {
+            let fileList = files.map(file => {
+                return { name: file };
+            });
+            res.send(fileList);
+        }
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Serveur en écoute sur le port ${port}`);
